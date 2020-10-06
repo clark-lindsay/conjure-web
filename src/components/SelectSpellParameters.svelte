951f003dec80e57a1a63eca8abf30e10d468d2a3
clark-lindsay <clarklindsay96@gmail.com>
2020-10-06 15:51:03 -0400

    Add feature: CR filtered by other options
    When the user selects options like the spell that they are casting, the
    source books that they are using, or the terrains that they want to
    summon creatures for, the challenge rating drop-down select will only
    show the challenge ratings that would summon a non-empty list of
    creatures.
    
    This may prove to be performance-degrading on devices with less
    computational power, and will have to be closely monitored with my
    testers.
    
    Additionally, the tests for the spell parameters selection component
    have been refactored to use the "describe..it..." format, instead of
    just using the "test" function, per the recommendation in the thoughtbot
    guides.

--- src/components/SelectSpellParameters.svelte
+++ src/components/SelectSpellParameters.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+  import {
+    conjureAnimals,
+    conjureWoodlandBeings,
+    conjureMinorElementals,
+  } from "conjure5e";
   import { writeSpellParameters } from "../stores/writeSpellParameters";
+  import { readSourcebooks } from "../stores/readSourcebooks";
 
   export let heading: string = "Spell Parameters";
 
@@ -8,8 +14,29 @@
     "Conjure Woodland Beings",
     "Conjure Minor Elementals",
   ];
+  let spell: ({}) => any[];
+  $: if ($writeSpellParameters.spellName === "Conjure Animals") {
+    spell = conjureAnimals;
+  } else if ($writeSpellParameters.spellName === "Conjure Woodland Beings") {
+    spell = conjureWoodlandBeings;
+  } else {
+    spell = conjureMinorElementals;
+  }
   const crOptions: number[] = [0, 0.125, 0.25, 0.5, 1, 2];
   const terrainOptions: string[] = ["Land", "Water", "Air"];
+
+  function forNonEmptyResults(challengeRating: number): boolean {
+    if (
+      spell({
+        terrains: $writeSpellParameters.terrains,
+        challengeRating,
+        sources: $readSourcebooks,
+      }).length > 0
+    ) {
+      return true;
+    }
+    return false;
+  }
 </script>
 
 <h2 class="text-blue-700 text-2xl mx-2">{heading}</h2>
@@ -27,19 +54,6 @@
     {/each}
   </select>
 
-  <label
-    for="challenge-rating-select"
-    class="text-gray-700 text-xl my-1 mx-2">Challenge Rating of Creatures</label>
-  <select
-    bind:value={$writeSpellParameters.challengeRating}
-    id="challenge-rating-select"
-    class="my-1 mx-2"
-    name="challenge-rating">
-    {#each crOptions as cr}
-      <option value={cr}>{cr}</option>
-    {/each}
-  </select>
-
   <h2 class="text-gray-700 text-xl my-1 mx-2">Terrains</h2>
   {#each terrainOptions as terrain}
     <label class="text-lg my-1 mx-2">
@@ -50,4 +64,26 @@
       {terrain}
     </label>
   {/each}
+
+  <label
+    for="challenge-rating-select"
+    class="text-gray-700 text-xl my-1 mx-2">Challenge Rating of Creatures</label>
+  <select
+    bind:value={$writeSpellParameters.challengeRating}
+    id="challenge-rating-select"
+    class="my-1 mx-2"
+    name="challenge-rating">
+    {#each crOptions.filter((cr) => {
+      if (spell({
+          terrains: $writeSpellParameters.terrains,
+          challengeRating: cr,
+          sources: $readSourcebooks,
+        }).length > 0) {
+        return true;
+      }
+      return false;
+    }) as cr}
+      <option value={cr}>{cr}</option>
+    {/each}
+  </select>
 </form>
