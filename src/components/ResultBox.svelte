8b665871dae30aeb189e582044c8d627efe82798
clark-lindsay <clarklindsay96@gmail.com>
2020-10-09 11:35:22 -0400

    layout edit for ResultBox

--- src/components/ResultBox.svelte
+++ src/components/ResultBox.svelte
@@ -20,7 +20,7 @@
     <h3 class="text-blue-700 text-lg mr-2">{heading}</h3>
     <div class="flex justify-between">
       {#if challengeRating}
-        <p class="text-blue-700 mr-2">CR{challengeRating}</p>
+        <p class="text-blue-700 mr-2">CR: {challengeRating}</p>
       {/if}
       {#if terrains.length !== 0}
         <p class="text-blue-700 mr-2">
